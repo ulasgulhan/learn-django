9832109bad7e5718c9d5404597a299e7d8496c69
komarite <ulasgulhan9@gmail.com>
2023-11-22 14:43:42 +0300

    templates and minor changes for views and ursl

--- examples/myapp/urls.py
+++ examples/myapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index),
-    path('details', views.details, name='details'),
     path('<int:category_id>', views.get_products_by_category_id),
-    path('<str:category>', views.get_products_by_category),
+    path('<str:category>', views.get_products_by_category, name='products_by_category'),
 ]
--- examples/myapp/views.py
+++ examples/myapp/views.py
@@ -2,29 +2,37 @@ from unicodedata import category
 from django.http import HttpResponseNotFound
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse
+from datetime import datetime
 
 data = {
-    'telefon': 'telefon kategorisindeki ürünler',
-    'bilgisayar': 'bilgisayar kategorisindeki ürünler',
-    'elektronik': 'elektronik kategorisindeki ürünler',
+    'telefon': ['samsung s20', 'samsung s21'],
+    'bilgisayar': ['laptop 1', 'laptop 2'],
+    'elektronik': [],
 }
 
 def index(request):
-    return HttpResponse('index')
+    categories = list(data.keys())
+    return render(request, 'myapp/index.html', {
+        'categories': categories
+    })
 
-def details(request):
-    return HttpResponse('details')
 
 def get_products_by_category_id(request, category_id):
     category_list = list(data.keys())
     if category_id > len(category_list):
         return HttpResponseNotFound('yanlış kategori seçimi')
-    redirect_text = category_list[category_id - 1]
-    return redirect('/products/' + redirect_text)
+    category_name = category_list[category_id - 1]
+    redirect_path = reverse('products_by_category', args=[category_name])
+    return redirect(redirect_path)
 
 def get_products_by_category(request, category):
     try:
-        category_text = data[category]
-        return HttpResponse(category_text)
+        products = data[category]
+        return render(request, 'myapp/products.html', {
+            'category': category,
+            'products': products,
+            'now': datetime.now()
+        })
     except:
-        return HttpResponseNotFound('yanlış kategori seçimi')
+        return HttpResponseNotFound(f'<h1>yanlış kategori seçimi</h1>')
