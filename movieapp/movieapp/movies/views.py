aca3573001c7fcfe8aedd15e08da561b1d1c82d7
komarite <ulasgulhan9@gmail.com>
2023-11-24 13:56:31 +0300

    template files

--- movieapp/movieapp/movies/views.py
+++ movieapp/movieapp/movies/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('index')
+    return render(request, 'index.html')
 
 def movies(request):
-    return HttpResponse('movies')
+    return render(request, 'movies.html')
 
 
 def movie_details(request, slug):
-    return HttpResponse('movie_details: ' + slug)
+    return render(request, 'movie_details.html', {
+        'slug': slug,
+    })
