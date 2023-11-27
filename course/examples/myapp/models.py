d4157315c513f7745eb60998571cacd2eabcac2a
komarite <ulasgulhan9@gmail.com>
2023-11-27 15:05:42 +0300

    Model update and CRUD test from python shell

--- course/examples/myapp/models.py
+++ course/examples/myapp/models.py
@@ -7,6 +7,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.CharField(max_length=200)
     imageUrl = models.CharField(max_length=50)
+    isActive = models.BooleanField(default=False)
+    category = models.CharField(max_length=50, null=True)
 
     def __str__(self) -> str:
         return f'{self.name} {self.price}'
