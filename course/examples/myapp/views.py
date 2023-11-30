a739fcb234f16c11af93152d7fe692ea651f9757
komarite <ulasgulhan9@gmail.com>
2023-11-30 13:12:16 +0300

    Get information from http POST method

--- course/examples/myapp/views.py
+++ course/examples/myapp/views.py
@@ -8,7 +8,7 @@ from django.db.models import Avg, Min, Max
 
 
 def index(request):
-    products = Product.objects.all().order_by('-price')
+    products = Product.objects.filter(isActive=True).order_by('-price')
 
     context = {
         'products': products,
@@ -17,11 +17,11 @@ def index(request):
 
 
 def list(request):
-    if request.GET['q'] and request.GET['q'] is not None:
+    if 'q' in request.GET and request.GET.get('q'):
         q = request.GET['q']
         products = Product.objects.filter(name__contains=q).order_by('-price')
     else:
-        return HttpResponseRedirect('/products')
+        products = Product.objects.all().order_by('-price')
     
     context = {
         'products': products,
@@ -31,6 +31,16 @@ def list(request):
 
 
 def create(request):
+    if request.method == 'POST':
+        product_name = request.POST['product_name']
+        price = request.POST['price']
+        description = request.POST['description']
+        slug = request.POST['slug']
+
+        p = Product(name = product_name, description = description, price = price, imageUrl = '1.jpg', slug = slug)
+        p.save()
+        return HttpResponseRedirect('list')
+    
     return render(request, 'create.html')
 
 
