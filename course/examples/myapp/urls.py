3b15e40f1edf492c54b57c16afbbc5e709027535
komarite <ulasgulhan9@gmail.com>
2023-11-30 12:55:28 +0300

    Prodcut search GET method and product create form added

--- course/examples/myapp/urls.py
+++ course/examples/myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index),
+    path('create', views.create),
+    path('list', views.list),
     path('<slug:slug>', views.details, name='product_details'),
-    path('<int:category_id>', views.get_products_by_category_id),
-    path('<str:category>', views.get_products_by_category, name='products_by_category'),
 ]
--- course/examples/myapp/views.py
+++ course/examples/myapp/views.py
@@ -1,28 +1,38 @@
 from unicodedata import category
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from datetime import datetime
 from .models import Product
 from django.db.models import Avg, Min, Max
 
-data = {
-    'telefon': ['samsung s20', 'samsung s21', 'iphone 12'],
-    'bilgisayar': ['laptop 1', 'laptop 2'],
-    'elektronik': [],
-}
 
 def index(request):
     products = Product.objects.all().order_by('-price')
-    product_count = Product.objects.filter(isActive=True).count()
-    price = Product.objects.filter(isActive=True).aggregate(Avg('price'), Min('price'), Max('price'))
 
     context = {
         'products': products,
-        'product_count': product_count,
-        'price': price
     }
     return render(request, 'index.html', context)
+
+
+def list(request):
+    if request.GET['q'] and request.GET['q'] is not None:
+        q = request.GET['q']
+        products = Product.objects.filter(name__contains=q).order_by('-price')
+    else:
+        return HttpResponseRedirect('/products')
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'list.html', context)
+
+
+def create(request):
+    return render(request, 'create.html')
+
 
 
 def details(request, slug):
@@ -33,23 +43,3 @@ def details(request, slug):
     }
 
     return render(request, 'details.html', context)
-
-
-def get_products_by_category_id(request, category_id):
-    category_list = list(data.keys())
-    if category_id > len(category_list):
-        return HttpResponseNotFound('yanlış kategori seçimi')
-    category_name = category_list[category_id - 1]
-    redirect_path = reverse('products_by_category', args=[category_name])
-    return redirect(redirect_path)
-
-def get_products_by_category(request, category):
-    try:
-        products = data[category]
-        return render(request, 'products.html', {
-            'category': category,
-            'products': products,
-            'now': datetime.now()
-        })
-    except:
-        return HttpResponseNotFound(f'<h1>yanlış kategori seçimi</h1>')
