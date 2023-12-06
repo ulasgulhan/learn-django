c3639d4d1ad616fd3231b89f6bf537b4f158805b
komarite <ulasgulhan9@gmail.com>
2023-12-06 14:17:00 +0300

    password change proccess with PasswordChangeFrom

--- course/examples/account/views.py
+++ course/examples/account/views.py
@@ -1,8 +1,8 @@
 import re
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 from .forms import LoginUserForm, NewUserForm
 
@@ -36,7 +36,7 @@ def register_request(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('index')
@@ -46,6 +46,20 @@ def register_request(request):
         form = NewUserForm()
         return render(request, 'account/register.html', {'form':form})
 
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Password Changed')
+            return redirect('change_password')
+        else:
+            return render(request, 'account/change_password.html', {'form':form})
+    else:
+        form = PasswordChangeForm(request.user)
+        return render(request, 'account/change_password.html', {'form':form})
 
 
 def logout_request(request):
